03d951cb3876e6cdc96721718118daa3a8857b7a
turuthivic <turuthivictor9@gmail.com>
2017-09-19 12:34:03 +0300

    final commit for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   helper_method :current_user, :logged_in?
 
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
